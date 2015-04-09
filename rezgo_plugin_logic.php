fffaa250fd6f948ea32b323610b7c865a1ecdac7
Pavel Khrebto <ridgeweber@gmail.com>
2015-04-09 17:33:08 +0300

    rewrite rules for details page in WP

--- rezgo_plugin_logic.php
+++ rezgo_plugin_logic.php
@@ -1,13 +1,40 @@
 <?php
 
-function rezgo_iframe()
+function rezgo_iframe($args)
 {
     global $wp;
     // the pagename can hide under a couple different names
     $wp_current_page = $wp->query_vars['name'];
+    if(empty($wp_cuurent_page)){
+        $wp_current_page = $wp->query_vars['pagename'];
+    }
+
+    parse_str($wp->matched_query, $matched_query);
+    foreach($matched_query as $k => $v) {
+        $_REQUEST[$k] = urldecode($v);
+    }
+
+    if(empty($_REQUEST['mode'])){
+        $_REQUEST['mode'] = 'index';
+    }
 
     rezgo_inlude_file('frame.php', array(
         'wp_current_page' => $wp_current_page,
     ));
 }
 
+function rezgo_add_rewrite_rules($wp_rewrite) {
+
+    $new_rules = array (
+
+        // tour details page (general)
+        '(.+?)/details/([0-9]+)/([^\/]+)/?$'
+        => 'index.php?pagename=$matches[1]&rezgo_page=tour_details&com=$matches[2]&mode=page_details',
+
+        // tour details page (date and option selected)
+        '(.+?)/details/([0-9]+)/([^\/]+)/([0-9]+)/([^\/]+)/?$'
+        => 'index.php?pagename=$matches[1]&rezgo_page=tour_details&com=$matches[2]&option=$matches[4]&date=$matches[5]&mode=page_details',
+    );
+
+    $wp_rewrite->rules = $new_rules + $wp_rewrite->rules;
+}
