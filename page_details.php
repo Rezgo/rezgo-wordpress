c945434e2eb9cc9fe187a5f6cd4bd67b58b338c0
Pavel Khrebto <ridgeweber@gmail.com>
2015-04-16 11:57:52 +0300

    error reporting fix + fix of configuration

--- page_details.php
+++ page_details.php
@@ -1,7 +1,6 @@
 <?php 
 	// any new page must start with the page_header, it will include the correct files
 	// so that the rezgo parser classes and functions will be available to your templates
-	error_reporting(0);
 	require('rezgo/include/page_header.php');
 	
 	// start a new instance of RezgoSite
--- rezgo/include/page_header.php
+++ rezgo/include/page_header.php
@@ -1,4 +1,5 @@
 <?php
+    require_once($_SERVER['DOCUMENT_ROOT'].'/wp-load.php');
 	require( 'config.rezgo.php' );
 	require( 'class.rezgo.php' );
 ?>
